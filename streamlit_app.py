e8b17d1353f2a454820dcdd386cc040645db5ef3
leonardo simoes <50172734+leonardoCaceres@users.noreply.github.com>
2022-05-16 11:10:30 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,4 +16,5 @@ In the meantime, below is an example of what you can do with just a few lines of
 """
 
 exchange_rates = pd.read_csv('entrada.csv')
-exchange_rates
+#exchange_rates
+st.line_chart(exchange_rates)
