c959544a751de3fb4917ca09444ec09f51d0125f
leonardo simoes <50172734+leonardoCaceres@users.noreply.github.com>
2022-05-16 12:00:21 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,9 @@ import math
 import pandas as pd
 import streamlit as st
 
-exchange_rates = pd.read_csv('entrada.csv')
-ahem = exchange_rates[['Periodo:', 'Total', 'Homens', 'Mulheres']].copy()
-st.line_chart(ahem)
+exchange_rates = pd.read_csv('entradaSemString.csv')
+#ahem = exchange_rates[['Periodo:', 'Total', 'Homens', 'Mulheres']].copy()
+st.line_chart(exchange_rates)
 
 """Mission529.py arquivo modificado para se adequar a taxa de analfabetsimo
  do brasil entre os anos de 2004 e 2019"""
